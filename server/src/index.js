b0f07379f6ee5ac887123dc94d0cb3611d6091dc
Adrian Meza <adrianme213@gmail.com>
2018-04-07 13:35:17 -0700

    [feat] - server responds with HelloWorld for / - server responds 404 for non-designated routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,26 @@
-// YOUR CODE HERE
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 8080;
+const logMethodUrl = function (req, res, next) {
+  if (req.url !== `/favicon.ico`) {
+    const date = new Date(Date.now());
+    console.log(`Time: ${date.toLocaleTimeString("en-US")} - Serving request ${req.method} at ${req.url}`);
+  }
+  next()
+}
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(logMethodUrl);
+
+
+// Route Handling
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/*', (req, res) => res.status(404).send('This page does not exist.'));
+
+// Set Port to Listen
+app.listen(PORT, () => console.log(`Fullstack app listening on ${PORT}`))
